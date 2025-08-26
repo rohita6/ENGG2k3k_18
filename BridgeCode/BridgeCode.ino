fad3f98ffd0d53dbf3dfe6074025c7f906c38b97
Jacob Liu <jacob.liu@students.mq.edu.au>
2025-08-26 10:30:30 +1000

    Updated Code 26/08

---
+++ BridgeCode/BridgeCode.ino
@@ -0,0 +1,31 @@
+#define TRIG_PIN 5
+#define ECHO_PIN 18
+#include WiFi.h
+float distance = 0
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  // Send a 10us pulse to trigger measurement
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Read the echo pulse
+  long duration = pulseIn(ECHO_PIN, HIGH);
+
+  // Calculate distance in cm (speed of sound: ~343 m/s)
+  distance = duration * 0.0343 / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(1000);
+}
