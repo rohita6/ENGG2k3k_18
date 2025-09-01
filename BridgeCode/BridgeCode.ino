55be9321bbaedb360f73bfa00c16d0575d581df1
Jacob Liu <jacob.liu@students.mq.edu.au>
2025-09-02 00:30:25 +1000

    Added some code for the ESP32 webpage for client UI

--- BridgeCode/BridgeCode.ino
+++ BridgeCode/BridgeCode.ino
@@ -1,16 +1,68 @@
-#define TRIG_PIN1 5
-#define ECHO_PIN1 18
-#define TRIG_PIN2 22
-#define ECHO_PIN2 23
+const int TRIG_PIN1 = 5;
+const int ECHO_PIN1 = 18;
+const int TRIG_PIN2 = 22;
+const int ECHO_PIN2 = 23;
+
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ssid = "YourWiFiName";
+const char* password = "YourWiFiPassword";
+
 float distance1 = 0;
 float distance2 = 0;
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(TRIG_PIN1, OUTPUT);
-  pinMode(ECHO_PIN1, INPUT);
-  pinMode(TRIG_PIN2, OUTPUT);
-  pinMode(ECHO_PIN2, INPUT);
+int ledPin = 2;      // GPIO2 onboard LED (blue)
+bool ledState = false;
+int counter = 0;
+
+void handleRoot() {
+  String page = R"rawliteral(
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>ESP32 AJAX Demo</title>
+    </head>
+    <body>
+      <h1>ESP32 AJAX Example</h1>
+      <p>LED State: <span id="led">OFF</span></p>
+      <button onclick="toggleLED()">Toggle LED</button>
+      <p>Counter: <span id="count">0</span></p>
+      <button onclick="getCounter()">Refresh Counter</button>
+
+      <script>
+        function toggleLED() {
+          fetch('/toggle')
+            .then(response => response.text())
+            .then(data => {
+              document.getElementById('led').innerText = data;
+            });
+        }
+
+        function getCounter() {
+          fetch('/counter')
+            .then(response => response.text())
+            .then(data => {
+              document.getElementById('count').innerText = data;
+            });
+        }
+      </script>
+    </body>
+    </html>
+  )rawliteral";
+
+  server.send(200, "text/html", page);
+}
+
+void handleToggle() {  
+  ledState = !ledState;
+  digitalWrite(ledPin, ledState ? HIGH : LOW);
+  server.send(200, "text/plain", ledState ? "ON" : "OFF");
+}
+
+void handleCounter() {
+  counter++;
+  server.send(200, "text/plain", String(counter));
 }
 
 void sensors() {
@@ -45,6 +97,35 @@ void sensors() {
   delay(1000);
 }
 
+void setup() {
+  Serial.begin(115200);
+  pinMode(TRIG_PIN1, OUTPUT);//Sensors
+  pinMode(ECHO_PIN1, INPUT);
+  pinMode(TRIG_PIN2, OUTPUT);
+  pinMode(ECHO_PIN2, INPUT);
+
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  WiFi.begin(ssid, password); // connect to wifi
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("Connected to WiFi! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Define routes
+  server.on("/", handleRoot);
+  server.on("/toggle", handleToggle);
+  server.on("/counter", handleCounter);
+
+  server.begin();
+}
+
+
+
 void loop() {
-  sensors();
+  sensors();//ping sensors
+  server.handleClient();
 }
