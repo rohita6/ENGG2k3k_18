7857830940974bed5b03f24f4d910352dba2f348
EnkhjinGanErdene <161665474+EnkhjinGanErdene@users.noreply.github.com>
2025-09-16 09:58:06 +1000

    Create MotorCode.ino

---
+++ MotorCode/MotorCode.ino
@@ -0,0 +1,77 @@
+/*  Automatic Bridge Control using Ultrasonic Sensor + L298N
+
+    - ENA -> GPIO 2
+    - IN1 -> GPIO 4
+    - IN2 -> GPIO 16
+    - Ultrasonic: TRIG -> GPIO 5, ECHO -> GPIO 18
+*/
+
+#define TRIG_PIN 5
+#define ECHO_PIN 18
+
+#define ENA 2
+#define IN1 4
+#define IN2 16
+
+// Motor control functions
+void openBridge() { 
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 200);  // PWM speed control (0–255)
+}
+
+void closeBridge() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, 200);
+}
+
+void stopMotor() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 0);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Sensor pins
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  // Motor driver pins
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  stopMotor(); // make sure motor is stopped initially
+}
+
+void loop() {
+  // --- Ultrasonic sensor measurement ---
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = duration * 0.0343 / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  // --- Control logic ---
+  if (distance > 0 && distance < 50) { 
+    Serial.println("Ship approaching, opening bridge...");
+    openBridge();
+  } else if (distance > 100) { 
+    Serial.println("Ship has passed, closing bridge...");
+    closeBridge();
+  } else {
+    stopMotor();
+  }
+
+  delay(500); // wait before next reading
+}
